d06518069b3b1d931c982b6b771d282916093fc7
VEBata <vkhloponya@yandex.ru>
2023-04-28 21:35:36 +0300

    required placeholder

--- scripts/validate.js
+++ scripts/validate.js
@@ -41,7 +41,7 @@ const toggleButtonState= (inputList, buttonElement, {inactiveButtonClass})=>{
   buttonElement.setAttribute('disabled', true);
 } else {
   buttonElement.classList.remove(inactiveButtonClass);
-  buttonElement.removeAttribute('disabled');
+  buttonElement.removeAttribute('disabled', true);
 }
 }
 
